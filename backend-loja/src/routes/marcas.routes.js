c23f2cf68e906dcf7dc97ec6e575f9538cbd3c2c
Vitor Hugo Almeida <vitorhugo_as@hotmail.com>
2025-09-16 13:31:55 -0300

    Update marcas.routes.js

--- backend-loja/src/routes/marcas.routes.js
+++ backend-loja/src/routes/marcas.routes.js
@@ -1,7 +1,19 @@
-// src/routes/marcas.routes.js
 import { Router } from "express";
-import { listarMarcas } from "../controllers/marcas.controller.js";
+import {
+  listarMarcas,
+  obterMarca,
+  criarMarca,
+  atualizarMarca,
+  deletarMarca,
+} from "../controllers/marcas.controller.js";
 
 const router = Router();
+
+// /api/marcas
 router.get("/", listarMarcas);
+router.get("/:id", obterMarca);
+router.post("/", criarMarca);
+router.put("/:id", atualizarMarca);
+router.delete("/:id", deletarMarca);
+
 export default router;
