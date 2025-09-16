2936db7fab4685f1f59d9b160ec28b1f1760b271
Vitor Hugo Almeida <vitorhugo_as@hotmail.com>
2025-09-16 13:31:41 -0300

    Update categorias.routes.js

--- backend-loja/src/routes/categorias.routes.js
+++ backend-loja/src/routes/categorias.routes.js
@@ -1,7 +1,19 @@
-// src/routes/categorias.routes.js
 import { Router } from "express";
-import { listarCategorias } from "../controllers/categorias.controller.js";
+import {
+  listarCategorias,
+  obterCategoria,
+  criarCategoria,
+  atualizarCategoria,
+  deletarCategoria,
+} from "../controllers/categorias.controller.js";
 
 const router = Router();
+
+// /api/categorias
 router.get("/", listarCategorias);
+router.get("/:id", obterCategoria);
+router.post("/", criarCategoria);
+router.put("/:id", atualizarCategoria);
+router.delete("/:id", deletarCategoria);
+
 export default router;
